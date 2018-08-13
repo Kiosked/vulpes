040ea43d075a3d0eb4e557883e9f15ae83c356e7
Perry Mitchell <perry@kiosked.com>
2018-08-13 14:23:40 +0300

    Add timelimit to job addition

--- source/Service.js
+++ source/Service.js
@@ -1,14 +1,18 @@
 const EventEmitter = require("eventemitter3");
 const merge = require("merge");
+const VError = require("verror");
+const ChannelQueue = require("@buttercup/channel-queue");
 const Storage = require("./storage/Storage.js");
 const MemoryStorage = require("./storage/MemoryStorage.js");
 const { generateEmptyJob } = require("./jobGeneration.js");
 const { selectJobs } = require("./jobQuery.js");
 const { updateJobChainForParents } = require("./jobMediation.js");
+const { getTimestamp } = require("./time.js");
 const {
     JOB_PRIORITY_HIGH,
     JOB_PRIORITY_LOW,
     JOB_PRIORITY_NORMAL,
+    JOB_STATUS_PENDING,
     JOB_TIMELIMIT_DEFAULT
 } = require("./symbols.js");
 
@@ -32,30 +36,49 @@ class Service extends EventEmitter {
         }
         this._storage = storage;
         this._timeLimit = JOB_TIMELIMIT_DEFAULT;
+        this._channelQueue = new ChannelQueue();
+    }
+
+    get jobQueue() {
+        return this._channelQueue.channel("job");
     }
 
     get storage() {
         return this._storage;
     }
 
+    get timeLimit() {
+        return this._timeLimit;
+    }
+
+    set timeLimit(tl) {
+        if (typeof tl !== "number" || tl < 0) {
+            throw new Error(`Failed setting time limit: Invalid number: ${tl}`);
+        }
+        this._timeLimit = tl;
+    }
+
     addJob(properties = {}) {
-        return Promise
-            .resolve()
-            .then(() => {
-                const job = merge.recursive(
-                    generateEmptyJob(),
-                    properties
-                );
-                return updateJobChainForParents(this, job);
-            })
-            .then(job => {
-                return this.storage
-                    .setItem(`job/${job.id}`, job)
-                    .then(() => {
-                        this.emit("jobAdded", { id: job.id });
-                        return job.id;
-                    });
-            });
+        return this.jobQueue.enqueue(() =>
+            Promise
+                .resolve()
+                .then(() => {
+                    const job = merge.recursive(
+                        generateEmptyJob(),
+                        { timeLimit: this.timeLimit },
+                        properties
+                    );
+                    return updateJobChainForParents(this, job);
+                })
+                .then(job => {
+                    return this.storage
+                        .setItem(`job/${job.id}`, job)
+                        .then(() => {
+                            this.emit("jobAdded", { id: job.id });
+                            return job.id;
+                        });
+                })
+        );
     }
 
     getJob(jobID) {
