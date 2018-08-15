e41098a83af1450ec46f0a4af4978aa6e4dfd2b5
Perry Mitchell <perry@perrymitchell.net>
2018-08-15 18:16:31 +0300

    Add async/await

--- test/index.js
+++ test/index.js
@@ -2,6 +2,9 @@ const chai = require("chai");
 const chaiAsPromised = require("chai-as-promised");
 const sinon = require("sinon");
 
+// Index is loaded so that babel-polyfill has a chance to get going
+require("../dist/index.js");
+
 const { expect } = chai;
 
 chai.use(chaiAsPromised);
