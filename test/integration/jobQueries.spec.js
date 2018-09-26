5626782ad6f2acff0738b58fed6c1100fd88b3c2
Perry Mitchell <perry@perrymitchell.net>
2018-09-26 11:57:02 +0300

    Add tests for sorting and limiting

--- test/integration/jobQueries.spec.js
+++ test/integration/jobQueries.spec.js
@@ -1,4 +1,5 @@
 const Service = require("../../dist/Service.js");
+const { JOB_STATUS_PENDING, JOB_STATUS_STOPPED } = require("../../dist/symbols.js");
 
 describe("Service", function() {
     beforeEach(function() {
@@ -7,8 +8,8 @@ describe("Service", function() {
             .initialise()
             .then(() =>
                 Promise.all([
-                    this.service.addJob({ data: { name: "test1" } }),
-                    this.service.addJob({ data: { name: "test2" } })
+                    this.service.addJob({ data: { name: "test1" }, type: "test1" }),
+                    this.service.addJob({ data: { name: "test2" }, type: "test2" })
                 ])
             )
             .then(([jobID1, jobID2]) => {
@@ -30,6 +31,20 @@ describe("Service", function() {
             return this.service.queryJobs({ "data.name": "test2" }).then(jobs => {
                 expect(jobs).to.have.lengthOf(1);
                 expect(jobs[0]).to.have.property("id", this.jobID2);
+            });
+        });
+
+        it("can limit jobs", function() {
+            return this.service.queryJobs({}, { limit: 1 }).then(jobs => {
+                expect(jobs).to.have.lengthOf(1);
+            });
+        });
+
+        it("can sort jobs", function() {
+            return this.service.queryJobs({}, { sort: "type", order: "desc" }).then(jobs => {
+                expect(jobs).to.have.lengthOf(2);
+                expect(jobs[0].type).to.equal("test2");
+                expect(jobs[1].type).to.equal("test1");
             });
         });
     });
